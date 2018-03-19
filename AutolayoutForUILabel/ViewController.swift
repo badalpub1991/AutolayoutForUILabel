898f9deb781c440476a8edc8d0573e62e5188f48
badal <badalpub1991@gmail.com>
2018-03-19 23:51:53 +0100

    Commited
    Final Commit

--- AutolayoutForUILabel/ViewController.swift
+++ AutolayoutForUILabel/ViewController.swift
@@ -10,15 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+    //Outlets
+    @IBOutlet weak var lbl1: UILabel!
+    @IBOutlet weak var lbl2: UILabel!
+    
+    //Variables
+    let labelString = "I'm making a custom UIView with 2 UILabel in it, with each UILabel having dynamic height. I'm unable to set constraints for dynamic height. (I'm new to Auto-Layout & iOS stuff)."
+    
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func increaseSizeUIButtonPressed(_ sender: Any) {
+        lbl1.text = labelString
+        lbl2.text = labelString
     }
+    
 
 
 }
