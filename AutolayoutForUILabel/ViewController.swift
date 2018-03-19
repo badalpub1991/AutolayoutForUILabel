1b71a26f25fb04e854fdf105e9d58cb3f2ffe666
badal <badalpub1991@gmail.com>
2018-03-19 23:48:47 +0100

    Initial Commit

---
+++ AutolayoutForUILabel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutolayoutForUILabel
+//
+//  Created by badal shah on 19/03/18.
+//  Copyright © 2018 badal shah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
